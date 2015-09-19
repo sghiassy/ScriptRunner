cb9dbad9ddc153a4ca667377ba106750443dca15
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-09-18 20:17:22 -0700

    Added Header. Barely started

--- index.ios.js
+++ index.ios.js
@@ -16,16 +16,12 @@ var ScriptRunner = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.header}>Script Runner</Text>
+        <View style={styles.center}>
+          <View style={styles.box}>
+            <Text>Shaheen</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -34,20 +30,22 @@ var ScriptRunner = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#131125',
+    padding: 10,
+    color: 'white',
+  },
+  center: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    color: 'white',
+    fontSize: 30,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  box: {
+    transform: [{rotate:'-30deg'}]
+  }
 });
 
 AppRegistry.registerComponent('ScriptRunner', () => ScriptRunner);
